016532a11284897020ae15267b8d8085a77f638f
Kerigan <kerigancreighton@gmail.com>
2024-09-28 18:05:06 -0500

    remove built thing, dont use docker container for building wired

--- wired/build.sh
+++ wired/build.sh
@@ -4,4 +4,15 @@ if [[ -d ./wired ]]; then
 	cd wired
 fi
 
-make wired
+#make wired
+# we don't need a docker container
+
+CC="${HOME}/.anki/vicos-sdk/dist/1.1.0-r04/prebuilt/bin/arm-oe-linux-gnueabi-clang -w" \
+CXX="${HOME}/.anki/vicos-sdk/dist/1.1.0-r04/prebuilt/bin/arm-oe-linux-gnueabi-clang++" \
+GOARCH=arm \
+GOARM=7 \
+GOOS=linux \
+go build  \
+--trimpath \
+-ldflags '-w -s -linkmode internal -extldflags "-static"' \
+-o build/wired
